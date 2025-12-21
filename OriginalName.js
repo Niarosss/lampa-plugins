a466d76f97d7ee69a289fa976d3a595cfbd017fe
Niarosss <suharevich.vadim@gmail.com>
2025-12-21 02:19:08 +0200

    change style

--- OriginalName.js
+++ OriginalName.js
@@ -5,9 +5,9 @@
     if (!render) return;
 
     $(".full-start-new__title", render).after(
-      `<div class="original_title" style="margin:1em 0;text-align:left;">
+      `<div class="original_title" style="margin:1.5em 0;text-align:left;">
          <div>
-           <div style='font-size:1.3em;'>Оригінальна назва: ${orig}</div>
+           <div style='font-size:1em;'>Оригінальна назва: ${orig}</div>
          </div>
        </div>`
     );
@@ -19,7 +19,7 @@
       if (e.type !== "complite" || !e.data.movie) return;
       $(".original_title", e.object.activity.render()).remove();
       $(".full-start-new__title", e.object.activity.render()).after(
-        '<div class="original_title" style="margin-top:-0.8em;text-align:left;"><div></div></div>'
+        '<div class="original_title" style="margin:1.5em 0;text-align:left;"><div></div></div>'
       );
       showTitles(e.data.movie);
     });
