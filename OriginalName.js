68c6ddbc4911122dc80462f53977bfcf5049e99a
Niarosss <suharevich.vadim@gmail.com>
2025-12-21 02:16:56 +0200

    and another

--- OriginalName.js
+++ OriginalName.js
@@ -5,7 +5,7 @@
     if (!render) return;
 
     $(".full-start-new__title", render).after(
-      `<div class="original_title" style="margin-top:2em;text-align:left;">
+      `<div class="original_title" style="margin:1em 0;text-align:left;">
          <div>
            <div style='font-size:1.3em;'>Оригінальна назва: ${orig}</div>
          </div>
