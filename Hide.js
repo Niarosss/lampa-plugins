056c046bb960268c3fa7c8ef4cf77995a4e1a8c9
Niarosss <suharevich.vadim@gmail.com>
2025-12-21 15:09:51 +0200

    .

--- Hide.js
+++ Hide.js
@@ -186,6 +186,10 @@
   var resetIcon =
     '<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path><path d="M3 3v5h5"></path></svg>';
 
+  // Иконка часов
+  var timeIcon =
+    '<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>';
+
   // Инициализируем плагины
   function startPlugin() {
     var manifest = {
@@ -637,7 +641,9 @@
 
           // Получаем иконку элемента
           var icon = "";
-          if ($item.find("svg").length) {
+          if (id.includes("head__time")) {
+            icon = timeIcon;
+          } else if ($item.find("svg").length) {
             icon = $item.html();
           } else if ($item.find("img").length) {
             icon =
@@ -650,6 +656,7 @@
 
           // Определяем название элемента по классам
           var titleKey = "";
+          var title = "";
           if (id.includes("open--search")) {
             titleKey = "head_action_search";
           } else if (id.includes("open--broadcast")) {
@@ -669,8 +676,7 @@
           } else {
             titleKey = "no_name";
           }
-
-          var title = Lampa.Lang.translate(titleKey);
+          title = Lampa.Lang.translate(titleKey);
 
           Lampa.SettingsApi.addParam({
             component: "menu_filter",
