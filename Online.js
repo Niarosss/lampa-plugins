9be21fcf104973f28ad38849170badd43a554753
Niarosss <suharevich.vadim@gmail.com>
2026-01-07 21:54:51 +0200

    fix

--- Online.js
+++ Online.js
@@ -345,7 +345,7 @@
       version: "1.0.0",
       name: "Online View with Sources",
       component: "online_view_ws",
-      onContextLauch: function onContextLauch(object) {
+      onLauch: function (object) {
         Lampa.Component.add("online_view_ws", component);
         Lampa.Activity.push({
           url: "",
@@ -356,24 +356,19 @@
       },
     };
 
-    Lampa.Manifest.plugins = manifest;
+    Lampa.Manifest.plugins.push(manifest);
 
     var button =
       '<div class="full-start__button selector view--online"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24"><path d="M8 5v14l11-7z"/></svg><span>#{online_watch_plugin}</span></div>';
 
     function addButton(e) {
-      if (e.object.activity.render().find(".view--online").length) return;
+      var render = e.activity || e.object.activity;
+      if (render.render().find(".view--online").length) return;
       var btn = $(Lampa.Lang.translate(button));
       btn.on("hover:enter", function () {
-        Lampa.Component.add("online_view_ws", component);
-        Lampa.Activity.push({
-          url: "",
-          title: "Перегляд онлайн",
-          component: "online_view_ws",
-          movie: e.data.movie,
-        });
+        manifest.onLauch(e.data.movie);
       });
-      e.object.activity.render().find(".view--torrent").after(btn);
+      render.render().find(".view--torrent").after(btn);
     }
 
     Lampa.Listener.follow("full", function (e) {
