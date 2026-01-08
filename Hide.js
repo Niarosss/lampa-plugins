393ce3f13db444c0eefc44ec48f9a4a95f554040
Niarosss <suharevich.vadim@gmail.com>
2026-01-08 14:30:31 +0200

    fix icon

--- Hide.js
+++ Hide.js
@@ -153,7 +153,10 @@
 
   // Іконка ока для налаштувань
   var eyeIcon =
-    '<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>';
+    '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+    '<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>' +
+    '<circle cx="12" cy="12" r="3" stroke="white" stroke-width="2"/>' +
+    "</svg>";
 
   // Іконка для кнопки "Показати все"
   var resetIcon =
@@ -290,7 +293,7 @@
       component: "menu_filter",
       name: Lampa.Lang.translate("menu_items_hide"),
       description: Lampa.Lang.translate("plugin_description"),
-      icon: '<div class="menu__ico">' + eyeIcon + "</div>",
+      icon: eyeIcon,
     });
 
     // Головне вікно налаштувань
